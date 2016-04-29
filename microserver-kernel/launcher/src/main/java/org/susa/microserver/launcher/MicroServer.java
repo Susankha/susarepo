04900ae03a79929403b27f8b6fe3c81f6d0e8576
Susankha <susankhanirmala@gmail.com>
2016-04-28 23:51:02 -0700

    request and responce processing completed

--- microserver-kernel/launcher/src/main/java/org/susa/microserver/launcher/MicroServer.java
+++ microserver-kernel/launcher/src/main/java/org/susa/microserver/launcher/MicroServer.java
@@ -1,18 +1,22 @@
 package org.susa.microserver.launcher;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by susankha on 4/21/16.
  */
 public class MicroServer extends Thread {
 
+    private static final Logger log = Logger.getLogger(MicroServer.class.getName());
     private int serverPort = 6066;
     private Thread runningThread;
     private boolean isStoped = false;
@@ -29,8 +33,6 @@ public class MicroServer extends Thread {
             this.runningThread = Thread.currentThread();
         }
 
-        Socket socket;
-
         try {
             serverSocket = new ServerSocket(this.serverPort);
         } catch (IOException e) {
@@ -38,15 +40,16 @@ public class MicroServer extends Thread {
         }
 
         while (!isStoped) {
+            Socket socket;
             try {
-                System.out.println(" Server is listening on port " + serverSocket.getLocalPort() + " ....");
+                log.info(" Server is listening on port " + serverSocket.getLocalPort() + " ....");
                 socket = serverSocket.accept();
-                System.out.println(" Server is connected to " + socket.getRemoteSocketAddress());
-                processclientRequest(socket);
-                System.out.println(" Thank you !!!");
-                socket.close();
+                socket.setKeepAlive(true);
+                log.info(" Server is connected to " + socket.getRemoteSocketAddress());
+                processClientRequest(socket);
+                log.info(" request done");
             } catch (IOException e) {
-                e.printStackTrace();
+                log.log(Level.SEVERE, " IO exception occured while reading the request  ", e);
             }
 
         }
@@ -56,44 +59,60 @@ public class MicroServer extends Thread {
      * @param conectedSocket
      * @throws IOException
      */
-    public void processclientRequest(Socket conectedSocket) throws IOException {
+    public void processClientRequest(Socket conectedSocket) throws IOException {
 
         InputStream inputStream = conectedSocket.getInputStream();
-        readInput(inputStream);
-
+        if (inputStream != null) {
+            readInput(inputStream);
+        }
         OutputStream outputStream = conectedSocket.getOutputStream();
-        writeOutput(outputStream);
-        System.out.println(" Request Proceed !!!! ");
+        if (outputStream != null) {
+            writeOutput(outputStream);
+        }
 
-    }
-
-    public void readInput(InputStream inputStream) throws IOException {
-
-        BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
-        String inputLine = null;
         try {
-            inputLine = inputReader.readLine();
-            while (inputLine != null || inputLine == "") {
-                System.out.println(" *** input *** = " + inputLine);
-                inputLine = inputReader.readLine();
-            }
+            inputStream.close();
+            outputStream.close();
+            conectedSocket.close();
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null)
-                inputStream.close();
-            if (inputReader != null)
-                inputReader.close();
+            log.log(Level.SEVERE, " IO exception occured while closing the socket connection  ", e);
         }
 
     }
 
-    public void writeOutput(OutputStream outputStream) throws IOException {
+    /**
+     * @param inputStream
+     * @throws IOException
+     */
+    public void readInput(InputStream inputStream) {
 
-        outputStream.write(("\"HTTP/1.1 200 OK\\n\\n<html><body>\" " + " Welcome to the Wso2 !!! " + "</body></html>")
-                .getBytes());
-        outputStream.flush();
-        outputStream.close();
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder requestContent = new StringBuilder("");
+        try {
+            while (inputReader.ready()) {
+                requestContent.append((char) inputReader.read());
+            }
+            log.info(" Request content : " + requestContent.toString());
+        } catch (IOException e) {
+            log.log(Level.SEVERE, " IO exception occured while processing the request  ", e);
+        }
+
+    }
+
+    /**
+     * @param outputStream
+     * @throws IOException
+     */
+    public void writeOutput(OutputStream outputStream) {
+
+        try {
+            outputStream
+                    .write(("\"HTTP/1.1 200 OK\\n\\n<html><body>\" " + " Welcome to the Wso2 !!! " + "</body></html>")
+                            .getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, " IO exception occured while processing the response  ", e);
+        }
     }
 
 }
